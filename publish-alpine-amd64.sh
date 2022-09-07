6054c171c7f8228f0999563ecafad26481f375f6
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2022-09-07 16:28:42 +0300

    fix

--- publish-alpine-amd64.sh
+++ publish-alpine-amd64.sh
@@ -6,11 +6,12 @@ set -e
 [ -z "$GITHUB_SHA" ] && (echo "\$GITHUB_SHA is empty"; exit 1)
 [ -z "$GITHUB_REF" ] && (echo "\$GITHUB_REF is empty"; exit 1)
 
-BUILD_IMAGE_VERSION="$(printf '%s' "$GITHUB_SHA" | cut -c -8)-alpine-amd64"
+COMMIT_IMAGE="docker.io/makasim/build-image:$(printf '%s' "$GITHUB_SHA" | cut -c -8)-alpine-amd64"
+BRANCH_IMAGE="docker.io/makasim/build-image:${GITHUB_REF##*/}-alpine-amd64"
 
-./build.sh "alpine/amd64/Dockerfile" "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}"
+./build.sh "alpine/amd64/Dockerfile" "${COMMIT_IMAGE}"
 
-docker tag "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}" "docker.io/makasim/build-image:${GITHUB_REF##*/}"
-docker push "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}"
-docker push "docker.io/makasim/build-image:${GITHUB_REF##*/}"
+docker tag "${COMMIT_IMAGE}" "${BRANCH_IMAGE}"
+docker push "docker.io/makasim/build-image:${COMMIT_IMAGE}"
+docker push "docker.io/makasim/build-image:${BRANCH_IMAGE}"
 
--- publish-debian-amd64.sh
+++ publish-debian-amd64.sh
@@ -12,3 +12,12 @@ BUILD_IMAGE_VERSION="$(printf '%s' "$GITHUB_SHA" | cut -c -8)-debian-amd64"
 docker tag "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}" "docker.io/makasim/build-image:${GITHUB_REF##*/}"
 docker push "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}"
 docker push "docker.io/makasim/build-image:${GITHUB_REF##*/}"
+
+COMMIT_IMAGE="docker.io/makasim/build-image:$(printf '%s' "$GITHUB_SHA" | cut -c -8)-debian-amd64"
+BRANCH_IMAGE="docker.io/makasim/build-image:${GITHUB_REF##*/}-debian-amd64"
+
+./build.sh "debian/amd64/Dockerfile" "${COMMIT_IMAGE}"
+
+docker tag "${COMMIT_IMAGE}" "${BRANCH_IMAGE}"
+docker push "docker.io/makasim/build-image:${COMMIT_IMAGE}"
+docker push "docker.io/makasim/build-image:${BRANCH_IMAGE}"
