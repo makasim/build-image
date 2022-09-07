0472c56687c8e89578906afe0797843278f93ddd
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2022-09-07 11:40:15 +0300

    upd

---
+++ publish-debian-arm64.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -x
+set -e
+
+[ -z "$GITHUB_SHA" ] && (echo "\$GITHUB_SHA is empty"; exit 1)
+
+BUILD_IMAGE_VERSION="$(printf '%s' "$GITHUB_SHA" | cut -c -8)-debian-arm64"
+
+./build "debian/amd64/Dockerfile" "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}"
+
+docker push "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}"
