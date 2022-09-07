82cc52782867f576a4740eaf35510bc5d1b2a9d6
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2022-09-07 13:27:12 +0300

    add alpine build, fix typo in debian build

--- publish-alpine-amd64.sh
+++ publish-alpine-amd64.sh
@@ -5,8 +5,8 @@ set -e
 
 [ -z "$GITHUB_SHA" ] && (echo "\$GITHUB_SHA is empty"; exit 1)
 
-BUILD_IMAGE_VERSION="$(printf '%s' "$GITHUB_SHA" | cut -c -8)-debian-amd64"
+BUILD_IMAGE_VERSION="$(printf '%s' "$GITHUB_SHA" | cut -c -8)-alpine-amd64"
 
-./build.sh "debian/amd64/Dockerfile" "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}"
+./build.sh "alpine/amd64/Dockerfile" "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}"
 
 docker push "docker.io/makasim/build-image:${BUILD_IMAGE_VERSION}"
